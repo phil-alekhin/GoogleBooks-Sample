a0f017af2100924449fc6259f252c1861c2e2c40
Philip Alekhin <phil.alekhin@gmail.com>
2018-05-20 00:02:58 +0300

    Add BookTableViewCell

---
+++ GoogleBooks/Classes/PresentationLayer/UserStories/BookListUserStory/Modules/BookListModule/View/Cells/BookCell/BookTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookTableViewCell.swift
+//  GoogleBooks
+//
+//  Created by Philip Alekhin on 19/05/2018.
+//  Copyright © 2018 Philip Alekhin. All rights reserved.
+//
+
+import UIKit
+
+final class BookTableViewCell: UITableViewCell {
+    @IBOutlet var thumbnailImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var publishedDateLabel: UILabel!
+}
