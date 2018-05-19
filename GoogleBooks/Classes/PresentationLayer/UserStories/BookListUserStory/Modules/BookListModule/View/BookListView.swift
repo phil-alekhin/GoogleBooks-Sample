5963b23f244e7225a47ba29a2ae7a46870340c2e
Philip Alekhin <phil.alekhin@gmail.com>
2018-05-19 23:40:09 +0300

    Add BookListPresenter and BookListView protocol

---
+++ GoogleBooks/Classes/PresentationLayer/UserStories/BookListUserStory/Modules/BookListModule/View/BookListView.swift
@@ -0,0 +1,9 @@
+//
+//  BooksListView.swift
+//  GoogleBooks
+//
+//  Created by Philip Alekhin on 19/05/2018.
+//  Copyright © 2018 Philip Alekhin. All rights reserved.
+//
+
+protocol BookListView: class {}
