73beb62bbf51e55836db2cb5fac976284688f17d
Philip Alekhin <phil.alekhin@gmail.com>
2018-05-19 16:50:37 +0300

    Add GetBooksUseCase protocol and Book entity

---
+++ GoogleBooks/Classes/Entities/Book.swift
@@ -0,0 +1,34 @@
+//
+//  Book.swift
+//  GoogleBooks
+//
+//  Created by Philip Alekhin on 18/05/2018.
+//  Copyright © 2018 Philip Alekhin. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Decodable {
+    struct BookThumbnails: Decodable {
+        let smallThumbnail: URL
+        let thumbnail: URL
+    }
+
+    let title: String
+    var description: String?
+    var authors: [String]?
+
+    var publisher: String?
+    var publishedDate: String?
+
+    let thumbnails: BookThumbnails
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case authors
+        case publisher
+        case publishedDate
+        case description
+        case thumbnails = "imageLinks"
+    }
+}
