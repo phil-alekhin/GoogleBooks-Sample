4fb1a0ca1d7d45a5f8d759baedeb29bb26ab8191
Philip Alekhin <phil.alekhin@gmail.com>
2018-05-20 22:47:00 +0300

    Add BooksDetailsViewController and storyboard

---
+++ GoogleBooks/Classes/PresentationLayer/UserStories/BookDetailsUserStory/Modules/BookDetails/View/BookDetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BookDetailsViewController.swift
+//  GoogleBooks
+//
+//  Created by Philip Alekhin on 20/05/2018.
+//  Copyright © 2018 Philip Alekhin. All rights reserved.
+//
+
+import UIKit
+
+final class BookDetailsViewController: UIViewController {
+
+    // MARK: IBOutlets
+
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
