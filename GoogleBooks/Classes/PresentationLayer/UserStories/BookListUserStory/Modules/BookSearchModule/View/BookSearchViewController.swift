958bc564b9215d4a96db712258a5ecc399be6c92
Philip Alekhin <phil.alekhin@gmail.com>
2018-05-20 19:45:47 +0300

    Add an empty BookSearchViewController

---
+++ GoogleBooks/Classes/PresentationLayer/UserStories/BookListUserStory/Modules/BookSearchModule/View/BookSearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BookSearchViewController.swift
+//  GoogleBooks
+//
+//  Created by Philip Alekhin on 20/05/2018.
+//  Copyright © 2018 Philip Alekhin. All rights reserved.
+//
+
+import UIKit
+
+final class BookSearchViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
