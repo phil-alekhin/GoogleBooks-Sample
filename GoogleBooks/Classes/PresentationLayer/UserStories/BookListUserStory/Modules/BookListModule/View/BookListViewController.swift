787d2e13c8685b8b6bf270817ba3d6e837533104
Philip Alekhin <phil.alekhin@gmail.com>
2018-05-21 10:30:49 +0300

    Fix layout of table views and search bar

--- GoogleBooks/Classes/PresentationLayer/UserStories/BookListUserStory/Modules/BookListModule/View/BookListViewController.swift
+++ GoogleBooks/Classes/PresentationLayer/UserStories/BookListUserStory/Modules/BookListModule/View/BookListViewController.swift
@@ -55,7 +55,7 @@ final class BookListViewController: UIViewController {
             navigationItem.searchController = searchController
             navigationItem.hidesSearchBarWhenScrolling = false
         } else {
-            tableView.tableHeaderView = searchController.searchBar
+            navigationItem.titleView = searchController.searchBar
         }
 
         definesPresentationContext = true
